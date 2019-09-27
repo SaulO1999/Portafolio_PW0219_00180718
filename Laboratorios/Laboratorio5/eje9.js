de9ca478a643c2889f913efd6bd96aaa1ad348d3
SaulO1999 <00180718@uca.edu.sv>
2019-09-27 10:45:37 -0600

    ADD:ejercicio9

--- Laboratorios/Laboratorio5/eje9.js
+++ Laboratorios/Laboratorio5/eje9.js
@@ -1,1 +1,10 @@
-var radio = window.prompt("")
+var radio = window.prompt("Ingrese el radio")
+function area(radio){
+    if(radio < 0){
+        return -1
+    }else{
+        let area = Math.pow(radio,2)*3.14;
+        return area;
+    }
+
+}
